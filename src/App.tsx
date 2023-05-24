9508902085008f3db934c07e708b191034b71007
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-05-24 11:00:28 +0300

    feat: ToDoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todolist from './Todolist';
+
+let task1 = [
+  { id: 1, title: 'CSS', isDone: true },
+  { id: 2, title: 'JS', isDone: true },
+  { id: 3, title: 'React', isDone: false }
+]
+
+let task2 = [
+  { id: 1, title: 'Terminator', isDone: true },
+  { id: 2, title: 'XXX', isDone: true },
+  { id: 3, title: 'Gentelmen', isDone: false }
+]
 
 function App() {
-  debugger;
+
   return (
     <div className="App">
-      <Todolist />
+      <Todolist title='What to Learn?' tasks={task1} />
+      <Todolist title='Movies' tasks={task2} />
+
     </div>
   );
 }
 
-function Todolist() {
 
-  return (
-    <h3>What to Learn</h3>
-  )
-
-}
 
 export default App;
